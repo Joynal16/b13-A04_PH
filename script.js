c6ad469c4f130d05c1a129ab997ce02768804077
Your Name <you@example.com>
2026-02-24 02:12:34 +0600

    first time

---
+++ script.js
@@ -0,0 +1,7 @@
+let total =document.getElementById("total");
+let interview =document.getElementById("interview");
+let rejected =document.getElementById("rejected");
+
+
+total.innerText='10';
+console.log(document.body)

baf42bf406b657a2b681023b6ef19e8090b5f9c7
Your Name <you@example.com>
2026-02-24 02:35:08 +0600

    second time

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ let interview =document.getElementById("interview");
 let rejected =document.getElementById("rejected");
 
 
-total.innerText='10';
-console.log(document.body)
+
+
+document.getElementById('mobile-btn')
+.addEventListener('click', function(){
+    // interview
+    let interview =document.getElementById("interview");
+    interview.innerText = parseInt(interview.innerText) + 1;
+    total.innerText = parseInt(total.innerText) + 1;
+
+    // rejected
+    let rejected =document.getElementById("rejected");
+    rejected.innerText = parseInt(rejected.innerText) + 1;
+    total.innerText = parseInt(total.innerText) + 1;
+})
